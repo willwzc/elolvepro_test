3dd1d3b429f50bf475f9a59508a6e53435d32f95
willwzc <95020365+willwzc@users.noreply.github.com>
2024-12-21 20:39:34 +0800

    Update plm_test.py

--- output_result/plm_test.py
+++ output_result/plm_test.py
@@ -1,3 +1,3 @@
 import os
 
-os.system('python C:/Users/wzcwi/EvolvePro/evolvepro/plm/esm/extract.py esm1b_t33_650M_UR50S C:/Users/wzcwi/EvolvePro/output_result/kelsic.fasta C:/Users/wzcwi/EvolvePro/output_result/kelsic_esm1b_t33_650M_UR50S --toks_per_batch 512 --include mean --concatenate_dir C:/Users/wzcwi/EvolvePro/output_result')
+os.system('python /home/zjlab/luca-data/will/EvolvePro/evolvepro/plm/esm/extract.py esm1b_t33_650M_UR50S /home/zjlab/luca-data/will/EvolvePro/output_result/kelsic.fasta /home/zjlab/luca-data/will/EvolvePro/output_result/kelsic_esm1b_t33_650M_UR50S --toks_per_batch 512 --include mean --concatenate_dir /home/zjlab/luca-data/will/EvolvePro/output_result')
