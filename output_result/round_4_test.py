f4f51233861b572354a3c25841e8aa9e3f9c981c
willwzc <95020365+willwzc@users.noreply.github.com>
2024-12-21 20:49:38 +0800

    Update round_4_test.py

--- output_result/round_4_test.py
+++ output_result/round_4_test.py
@@ -1,12 +1,12 @@
 from evolvepro.src.evolve import evolve_experimental
 
 protein_name = 'kelsic'
-embeddings_base_path = 'C:/Users/wzcwi/EvolvePro/output_result'
+embeddings_base_path = '/home/zjlab/luca-data/will/EvolvePro/output_result'
 embeddings_file_name = 'kelsic_esm1b_t33_650M_UR50S.csv'
-round_base_path = 'C:/Users/wzcwi/EvolvePro/output_result/rounds_data'
-wt_fasta_path = 'C:/Users/wzcwi/EvolvePro/output_result/kelsic_WT.fasta'
+round_base_path = '/home/zjlab/luca-data/will/EvolvePro/output_result/rounds_data'
+wt_fasta_path = '/home/zjlab/luca-data/will/EvolvePro/output_result/kelsic_WT.fasta'
 number_of_variants = 12
-output_dir = 'C:/Users/wzcwi/EvolvePro/output_result'
+output_dir = '/home/zjlab/luca-data/will/EvolvePro/output_result'
 rename_WT = False
 
 round_name = 'Round4'
