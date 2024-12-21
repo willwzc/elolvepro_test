aa082cd778da92d2ccb4b6995b04368a047bc837
willwzc <95020365+willwzc@users.noreply.github.com>
2024-12-21 20:38:37 +0800

    Update just_a_test.src.py

--- output_result/just_a_test.src.py
+++ output_result/just_a_test.src.py
@@ -1,5 +1,7 @@
 from evolvepro.src.process import generate_wt, generate_single_aa_mutants
-generate_wt('MAKEDNIEMQGTVLETLPNTMFRVELENGHVVTAHISGKMRKNYIRILTGDKVTVELTPYDLSKGRIVFRSR', output_file='C:/Users/wzcwi/EvolvePro/output_result/kelsic_WT.fasta')
-generate_single_aa_mutants('C:/Users/wzcwi/EvolvePro/output_result/kelsic_WT.fasta', output_file='C:/Users/wzcwi/EvolvePro/output_result/kelsic.fasta')
-input("Press Enter to exit...")
-exit()
+
+generate_wt('MAKEDNIEMQGTVLETLPNTMFRVELENGHVVTAHISGKMRKNYIRILTGDKVTVELTPYDLSKGRIVFRSR', 
+            output_file='/home/zjlab/luca-data/will/EvolvePro/output_result/kelsic_WT.fasta')
+
+generate_single_aa_mutants('/home/zjlab/luca-data/will/EvolvePro/output_result/kelsic_WT.fasta', 
+                           output_file='/home/zjlab/luca-data/will/EvolvePro/output_result/kelsic.fasta')
