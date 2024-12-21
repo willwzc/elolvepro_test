3a5802bb20525d113d5d79e91d66a723bba503ef
willwzc <wzcwill@163.com>
2024-12-21 19:40:36 +0800

    Add my folder

---
+++ output_result/plm_test.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('python C:/Users/wzcwi/EvolvePro/evolvepro/plm/esm/extract.py esm1b_t33_650M_UR50S C:/Users/wzcwi/EvolvePro/output_result/kelsic.fasta C:/Users/wzcwi/EvolvePro/output_result/kelsic_esm1b_t33_650M_UR50S --toks_per_batch 512 --include mean --concatenate_dir C:/Users/wzcwi/EvolvePro/output_result')
---
+++ output_result/suggest_mutant.src.py
@@ -0,0 +1,2 @@
+from evolvepro.src.process import suggest_initial_mutants
+suggest_initial_mutants('C:/Users/wzcwi/EvolvePro/output_result/kelsic.fasta', num_mutants=12, random_seed=42)
